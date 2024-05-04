0f3aef9d46c399de246d6e064ea6f48d3cc4edf9
Dany Gagnon <ddanygagnon@gmail.com>
2024-05-03 20:31:17 -0400

    fix: the formatting

--- src/queue.rs
+++ src/queue.rs
@@ -121,8 +121,14 @@ impl Queue {
         let mut con = self.client.get_connection().unwrap();
 
         while shutdown.try_recv().is_err() {
-            let Ok(payload) = con.blmove::<_, _, String>(&self.queues.pending, &self.queues.recovery, Direction::Right, Direction::Left, 5) else {
-                continue
+            let Ok(payload) = con.blmove::<_, _, String>(
+                &self.queues.pending,
+                &self.queues.recovery,
+                Direction::Right,
+                Direction::Left,
+                5,
+            ) else {
+                continue;
             };
 
             match (self.handler)(Job::from_string(&payload)?) {
